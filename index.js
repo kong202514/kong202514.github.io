659290c245245417562ea87bdf9e5fd5a0ed83c2
kong202514 <86356566+kong202514@users.noreply.github.com>
2023-12-28 17:08:52 +0700

    a

--- index.js
+++ index.js
@@ -1,64 +1,26 @@
+console.log(555);
+
+let a = ['วันอาทิตย์', 'วันจันทร์', 'วันอังคาร', 'วันพุธ', 'วันพฤหัสบดี', 'วันศุกร์', 'วันเสาร์ ']
 
 
-// var blok = document.querySelectorAll(".menu_bar")
+let as = [['อก', 'หลังแขน'], ['ไหล่', 'หน้าแขน'], 'ขา', 'พัก']
 
+let w = [
 
-// function add_class(event) {
-//     blok = event.target.style.backgroundColor = "#FFFFCC"
-//     blok = event.target.style.width = "20%"
-//     blok = event.target.style.height = "2em"
+    [['วันอาทิตย์'], ['อก'], ['หลังแขน']],
 
+    [['วันจันทร์'], ['ไหล่'], ['หน้าแขน']],
 
-// }
+    [['วันอังคาร'], ['ขา']],
 
+    [['วันพุธ'], ['พัก']],
 
-// // image001.png
+    [['วันพฤหัสบดี'], ['อก'], ['หลังแขน']],
 
-// function remove(event) {
-//     blok = event.target.style.backgroundColor = ""
-//     blok = event.target.style.width = ""
-//     blok = event.target.style.height = ""
+    [['วันศุกร์'], ['ไหล่'], ['หน้าแขน']],
 
-// }
+    [['วันเสาร์'], ['ขา']],
+]
 
+console.log(w[0][0], [1], [2]);
 
-
-// blok.forEach((a) => {
-//     a.addEventListener('mouseover', add_class)
-//     a.addEventListener('mouseout', remove)
-
-
-// })
-
-
-
-function includeHTML() {
-    var z, i, elmnt, file, xhttp;
-    /*loop through a collection of all HTML elements:*/
-    z = document.getElementsByTagName("*");
-    for (i = 0; i < z.length; i++) {
-        elmnt = z[i];
-        /*search for elements with a certain attribute:*/
-        file = elmnt.getAttribute("w3-include-html");
-        if (file) {
-            /*make an HTTP request using the attribute value as the file name:*/
-            xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function () {
-                if (this.readyState == 4) {
-                    if (this.status == 200) { elmnt.innerHTML = this.responseText; }
-                    if (this.status == 404) { elmnt.innerHTML = "Page not found."; }
-                    /*remove the attribute, and call this function once more:*/
-                    elmnt.removeAttribute("w3-include-html");
-                    includeHTML();
-                }
-            }
-            xhttp.open("GET", file, true);
-            xhttp.send();
-            /*exit the function:*/
-            return;
-        }
-    }
-};
-
-
-includeHTML();
