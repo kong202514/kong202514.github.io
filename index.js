0627ab960019d54b72733354e5db72554183c4bc
kong202514 <0635890449gong@gmail.com>
2023-12-29 18:22:37 +0700

    aaa

--- index.js
+++ index.js
@@ -1,69 +1,77 @@
 console.log(555);
 
-let a = ['วันอาทิตย์', 'วันจันทร์', 'วันอังคาร', 'วันพุธ', 'วันพฤหัสบดี', 'วันศุกร์', 'วันเสาร์ ']
+
 
 
 let as = [['อก', 'หลังแขน'], ['ไหล่', 'หน้าแขน'], 'ขา', 'พัก']
 let as2 = [
 
-    'อก',[4],
-    'หลังแขน' ,[2]   ,
-     'ไหล่',[4],
-    'หน้าแขน',[3],
+    'อก', [4],
+    'หลังแขน', [2],
+    'ไหล่', [4],
+    'หน้าแขน', [3],
     'ขา',
     'พัก']
 
 
 let w = [
 
-    [['วันอาทิตย์'], ['อก',4], ['หลังแขน',2]],
+    [['วันอาทิตย์'], ['อก', 4], ['หลังแขน', 2]],
 
-    [['วันจันทร์'], ['ไหล่',4], ['หน้าแขน',3]],
+    [['วันจันทร์'], ['ไหล่', 4], ['หน้าแขน', 3]],
 
     [['วันอังคาร'], ['ขา']],
 
     [['วันพุธ'], ['พัก']],
 
-    [['วันพฤหัสบดี'], ['อก',4], ['หลังแขน',2]],
+    [['วันพฤหัสบดี'], ['อก', 4], ['หลังแขน', 2]],
 
-    [['วันศุกร์'], ['ไหล่',4], ['หน้าแขน',3]],
+    [['วันศุกร์'], ['ไหล่', 4], ['หน้าแขน', 3]],
 
     [['วันเสาร์'], ['ขา']],
 ]
 
 
 
- 
+
 // รับอ้างถึงปุ่มและผลลัพธ์
-const buttons = document.querySelectorAll('button');
-const resultDiv = document.getElementById('ms');
+const all_buttons = document.querySelectorAll('button');
+const text_output = document.getElementById('ms');
 
 // กำหนดการทำงานเมื่อปุ่มถูกคลิก
-buttons.forEach(button => {
+all_buttons.forEach(button => {
     button.addEventListener('click', () => {
         // ดึงข้อมูลออกมาจากปุ่มที่ถูกคลิก
-        const buttonData = button.textContent;
-        
-        // แสดงข้อมูลที่ดึงออกมาใน div ผลลัพธ์
-        resultDiv.textContent = `ข้อมูลที่ดึงออกมา: ${buttonData}`;
+        const button_data = button.textContent;
+
+        let a = ['วันอาทิตย์', 'วันจันทร์', 'วันอังคาร', 'วันพุธ', 'วันพฤหัสบดี', 'วันศุกร์', 'วันเสาร์ ']
+
+        if (button_data === 'วันอาทิตย์') {
+            text_output.textContent = 'ข้อมูลที่ดึงออกมา:' + ' อก  หลังแขน  '
+        }
+
+        if (button_data === 'วันจันทร์') {
+            text_output.textContent = 'ข้อมูลที่ดึงออกมา:' + ' ไหล่  หน้าแขน   '
+        }
+        if (button_data === 'วันอังคาร') {
+            text_output.textContent = 'ข้อมูลที่ดึงออกมา:' + ' ขา   '
+        }
+        if (button_data === 'วันพุธ') {
+            text_output.textContent = 'ข้อมูลที่ดึงออกมา:' + 'พัก'
+        }
+        if (button_data === 'วันพฤหัสบดี') {
+            text_output.textContent = 'ข้อมูลที่ดึงออกมา:' + ' อก   หลังแขน'
+        }
+
+        if (button_data === 'วันศุกร์') {
+            text_output.textContent = 'ข้อมูลที่ดึงออกมา:' + 'ไหล่  หน้าแขน '
+        }
+        if (button_data === 'วันเสาร์') {
+            text_output.textContent = 'ข้อมูลที่ดึงออกมา:' + ' ขา '
+        }
+
+
     });
 });
 
 
-
-
-
-// // รับอ้างถึงปุ่มและผลลัพธ์
-// const buttons2 = document.querySelectorAll('button');
-// const resultDiv2 = document.getElementById('ms');
-
-// // กำหนดการทำงานเมื่อปุ่มถูกคลิก
-// buttons.forEach(button => {
-//     button.addEventListener('click', () => {
-//         // ดึงข้อมูลออกมาจากปุ่มที่ถูกคลิก
-//         const buttonData = button.textContent;
-        
-//         // แสดงข้อมูลที่ดึงออกมาใน div ผลลัพธ์
-//         resultDiv2.textContent = `ข้อมูลที่ดึงออกมา: ${buttonData}`;
-//     });
-// });
