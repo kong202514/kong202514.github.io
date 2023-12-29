bd18191027ecdea77e7a011f6a205e11b77adde2
kong202514 <0635890449gong@gmail.com>
2023-12-30 00:07:51 +0700

    45

--- index.js
+++ index.js
@@ -37,6 +37,22 @@ let w = [
 // รับอ้างถึงปุ่มและผลลัพธ์
 const all_buttons = document.querySelectorAll('button');
 const text_output = document.getElementById('ms');
+const bt0 = document.getElementById('bt0');
+const bt1 = document.getElementById('bt1');
+
+
+function ss() {
+
+    bt0.style.display = "none"
+    bt1.style.display = "none"
+}
+
+
+function aa() {
+
+    bt0.style.display = "show"
+    bt1.style.display = "show"
+}
 
 // กำหนดการทำงานเมื่อปุ่มถูกคลิก
 all_buttons.forEach(button => {
@@ -46,28 +62,115 @@ all_buttons.forEach(button => {
 
         let a = ['วันอาทิตย์', 'วันจันทร์', 'วันอังคาร', 'วันพุธ', 'วันพฤหัสบดี', 'วันศุกร์', 'วันเสาร์ ']
 
+
+
+
+
+        // if (condition) {
+
+        // } else if (condition) {
+
+        // } else if (condition) {
+
+        // } else if (condition) {
+
+        // } else if (condition) {
+
+        // } else if (condition) {
+
+        // } else if (condition) {
+
+        // } else if (condition) {
+
+        // } else if (condition) {
+
+        // } else if (condition) {
+
+        // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         if (button_data === 'วันอาทิตย์') {
+
+            bt0.textContent = "อก "
+            bt1.textContent = "หลังแขน "
             text_output.textContent = 'ข้อมูลที่ดึงออกมา:' + ' อก  หลังแขน  '
+            return
         }
 
         if (button_data === 'วันจันทร์') {
+
+
+            bt0.textContent = "ไหล่ "
+            bt1.textContent = "หน้าแขน "
+
             text_output.textContent = 'ข้อมูลที่ดึงออกมา:' + ' ไหล่  หน้าแขน   '
+            return
+
         }
         if (button_data === 'วันอังคาร') {
+
+
+            bt0.textContent = "ขา "
+
             text_output.textContent = 'ข้อมูลที่ดึงออกมา:' + ' ขา   '
+            return
+
         }
         if (button_data === 'วันพุธ') {
+
+
+            bt0.textContent = "พัก "
+            bt1.textContent = "พัก "
             text_output.textContent = 'ข้อมูลที่ดึงออกมา:' + 'พัก'
+
         }
         if (button_data === 'วันพฤหัสบดี') {
+
+
+
+
+
+            bt0.textContent = "อก "
+            bt1.textContent = "หลังแขน "
             text_output.textContent = 'ข้อมูลที่ดึงออกมา:' + ' อก   หลังแขน'
+            return
+
         }
 
         if (button_data === 'วันศุกร์') {
+            bt0.textContent = "ไหล่ "
+            bt1.textContent = "หน้าแขน "
             text_output.textContent = 'ข้อมูลที่ดึงออกมา:' + 'ไหล่  หน้าแขน '
+            return
+
         }
         if (button_data === 'วันเสาร์') {
+
+
+            bt0.textContent = "ขา "
+            bt1.textContent = "  "
             text_output.textContent = 'ข้อมูลที่ดึงออกมา:' + ' ขา '
+            return
+
         }
 
 
