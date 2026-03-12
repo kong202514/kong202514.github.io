8277f5af7793c15b814b82dfe6955453e753747a
kong202514 <86356566+kong202514@users.noreply.github.com>
2026-03-12 23:34:35 +0700

    w

--- src/page/len_js/extractUrl.js
+++ src/page/len_js/extractUrl.js
@@ -1,8 +1,1 @@
-function extractUrl(input) {
-    if (input.includes('<iframe')) {
-        const match = input.match(/src="([^"]+)"/);
-        return match ? match[1] : '';
-    }
-    return input;
-}
-export { extractUrl };
+ 
