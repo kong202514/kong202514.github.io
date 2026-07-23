50ad8cbab890d4c37f6d826350fc0bf9c68b55d5
kong202514 <86356566+kong202514@users.noreply.github.com>
2026-07-24 03:09:04 +0700

    sdd

--- src/js/server.js
+++ src/js/server.js
@@ -1,26 +1,73 @@
-function includeHTML() {
+// function includeHTML() {
+//     var z, i, elmnt, file, xhttp;
+//     z = document.getElementsByTagName("*");
+//     for (i = 0; i < z.length; i++) {
+//         elmnt = z[i];
+       
+//         file = elmnt.getAttribute("w3-include-html");
+//         if (file) {
+//             xhttp = new XMLHttpRequest();
+//             xhttp.onreadystatechange = function () {
+//                 if (this.readyState == 4) {
+//                     if (this.status == 200) { elmnt.innerHTML = this.responseText; }
+//                     if (this.status == 404) { elmnt.innerHTML = "Page not found."; }
+//                     elmnt.removeAttribute("w3-include-html");
+//                     includeHTML();
+//                 }
+//             }
+//             xhttp.open("GET", file, true);
+//             xhttp.send();
+//             return;
+//         }
+//     }
+// };
+ 
+
+// includeHTML();
+
+
+
+function includeHTML(callback) {
     var z, i, elmnt, file, xhttp;
     z = document.getElementsByTagName("*");
     for (i = 0; i < z.length; i++) {
         elmnt = z[i];
-       
         file = elmnt.getAttribute("w3-include-html");
         if (file) {
             xhttp = new XMLHttpRequest();
             xhttp.onreadystatechange = function () {
                 if (this.readyState == 4) {
-                    if (this.status == 200) { elmnt.innerHTML = this.responseText; }
-                    if (this.status == 404) { elmnt.innerHTML = "Page not found."; }
+                    if (this.status == 200) {
+                        elmnt.innerHTML = this.responseText;
+
+                        // รัน script tag ที่ติดมากับ HTML snippet
+                        var scripts = elmnt.getElementsByTagName("script");
+                        for (var j = 0; j < scripts.length; j++) {
+                            var script = document.createElement("script");
+                            if (scripts[j].src) {
+                                script.src = scripts[j].src;
+                            } else {
+                                script.text = scripts[j].innerHTML;
+                            }
+                            document.body.appendChild(script);
+                        }
+                    }
+                    if (this.status == 404) {
+                        elmnt.innerHTML = "Page not found.";
+                    }
                     elmnt.removeAttribute("w3-include-html");
-                    includeHTML();
+                    includeHTML(callback);
                 }
-            }
+            };
             xhttp.open("GET", file, true);
             xhttp.send();
             return;
         }
     }
-};
- 
+    if (callback) callback();
+}
 
-includeHTML();
+// เรียกใช้งานเมื่อ DOM โหลดเสร็จ
+document.addEventListener("DOMContentLoaded", function () {
+    includeHTML();
+});
